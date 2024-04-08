08ebfec672cd1fe478efada1cd380035578a15dd
Eneias Raul <eneias.raul@academico.ifpb.edu.br>
2024-04-07 21:27:16 -0300

    Aidionando função de filtrar

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.botao-adicionar');
     const input = document.querySelector('.entrada-tarefa');
     const listaCompleta = document.querySelector('.lista-tarefa');
+    const botaoConcluidos = document.querySelector('#botao-concluidos');
+    const botaoPendentes = document.querySelector('#botao-pendentes');
 
     button.addEventListener('click', adicionarTarefa);
+    botaoConcluidos.addEventListener('click', mostrarConcluidos);
+    botaoPendentes.addEventListener('click', mostrarPendentes);
 
     let listaDeItens = [];
 
@@ -57,5 +61,33 @@ document.addEventListener('DOMContentLoaded', function() {
         mostrarTarefa();
     }
 
-    localStorage.setItem('listaDeTarefas', JSON.stringify (adicionarTaref));
+    function mostrarConcluidos() {
+        const tarefasConcluidas = listaDeItens.filter(item => item.concluida);
+
+        listaCompleta.innerHTML = '';
+
+        tarefasConcluidas.forEach((item, posicao) => {
+            listaCompleta.innerHTML += 
+                `<li class="tarefa feito">
+                    <img src="img/checked.svg" alt="area-de-check">
+                    <img src="img/bin.svg" alt="area-de-excluir">
+                    <p>${item.tarefa}</p>
+                </li>`;
+        });
+    }
+
+    function mostrarPendentes() {
+        const tarefasPendentes = listaDeItens.filter(item => !item.concluida);
+
+        listaCompleta.innerHTML = '';
+
+        tarefasPendentes.forEach((item, posicao) => {
+            listaCompleta.innerHTML += 
+                `<li class="tarefa">
+                    <img src="img/checked.svg" alt="area-de-check">
+                    <img src="img/bin.svg" alt="area-de-excluir">
+                    <p>${item.tarefa}</p>
+                </li>`;
+        });
+    }
 });
